16160e6d8a301c5e19829e75473fd9e7c846445e
GuanMing Chee <guanmingchee11@gmail.com>
2026-05-06 18:41:39 +0800

    logic behind winning

--- tictactoe.js
+++ tictactoe.js
@@ -21,6 +21,29 @@ let player1=createPlayer(11);
 let player2=createPlayer(22);
 
 
-function checkWinOrTie(){
-    
+function checkWinOrTie(board){
+    let winningCondition=[
+        //row
+        [[0,0],[0,1],[0,2]],
+        [[1,0],[1,1],[1,2]],
+        [[2,0],[2,1],[2,2]],
+        //col
+        [[0,0],[1,0],[2,0]],
+        [[0,1],[1,1],[2,1]],
+        [[0,2],[1,2],[2,2]],
+        //diag
+        [[0,0],[1,1],[2,2]],
+        [[2,0],[1,1],[0,2]],
+    ]
+
+    for (let w of winningCondition){
+        //let [a,b,c]=w
+        if (board[w[0][0]][w[0][1]]!=='i' && board[w[0][0]][w[0][1]]==board[w[1][0]][w[1][1]] && board[w[0][0]][w[0][1]]==board[w[2][0]][w[2][1]]){
+            return board[w[0][0]][w[0][1]];
+        }
+    }
+
+    if (board.every(r => !(r.includes('i')))){
+        return "Tie";
+    }
 }
